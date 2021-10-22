9fb4470e6f139c2285b4793bdf2e58d50f9c13c9
UJWAL-REDDY <83937161+UJWAL-REDDY@users.noreply.github.com>
2021-10-22 12:51:44 +0530

    Add files via upload

---
+++ Lab 1/example.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+public class example{
+public static void main(String[]args){
+int num;
+num = 100;
+System.out.println ("this is num=num");
+num = num*2;
+System.out.println ("this value of num*2 is");
+System.out.println(num);
+}
+}
